dfd38fa7314e9957c2bb0838806c7bdf3a19cf3f
q.chen <q7but@qq.com>
2021-03-28 00:48:29 +0800

    update

--- do.sh
+++ do.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
 
-addGitCommit() {
-  git add .
-  git commit -m '${1} dist 打包'
-}
+if  [ -d  "dist"  ]
+then
+rm -rf dist
+fi
 
 branch=$(git branch | grep '*' | sed 's/* //') 
-
-echo ${branch}
+commitText=''
+echo "当前分支 =====>> ${branch}"
+echo "注意: 目前设置为 master分支下 production 模式打包, test分支下 development 模式打包"
 
 if [ ${branch} = 'master' ]
 then 
-  echo 'master 生产打包'
+  commitText="deploy ${branch} 分支下,production模式打包部署"
   yarn run build:prod
-  cd ./dist
-  touch prod.txt
-  addGitCommit ${branch}
 else
-  echo 'test 打包'
+  commitText="deploy ${branch} 分支下,development模式打包部署"
   yarn run build:test
-  cd ./dist
-  touch test.txt
-  addGitCommit ${branch}
 fi
+
+# cp -r buildFile/. dist/
+
+echo $commitText $branch
+
+git add .
+git commit -m $commitText
+
+if [ ${branch} = 'master' ]
+then 
+  echo 'master 分支，打tag部署===>>'
+  date=$(date +%Y%m%d%H%M)
+  git tag -a tag$date -m $date
+  echo "当前TAG= "tag$date  
+fi
+
